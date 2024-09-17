5199880351479af90a2674f83669eb50beb8ed80
nunosalg <nunosalgueiro23@gmail.com>
2024-09-17 14:05:40 +0100

    Category, Exercise and Workout repositories

---
+++ FitnessHub/FitnessHub/Data/Repositories/IExerciseRepository.cs
@@ -0,0 +1,8 @@
+﻿using FitnessHub.Data.Entities.GymMachines;
+
+namespace FitnessHub.Data.Repositories
+{
+    public interface IExerciseRepository : IGenericRepository<Exercise>
+    {
+    }
+}
---
+++ FitnessHub/FitnessHub/Data/Repositories/WorkoutRepository.cs
@@ -0,0 +1,14 @@
+﻿using FitnessHub.Data.Entities.GymMachines;
+
+namespace FitnessHub.Data.Repositories
+{
+    public class WorkoutRepository : GenericRepository<Workout>, IWorkoutRepository
+    {
+        private readonly DataContext _context;
+
+        public WorkoutRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
