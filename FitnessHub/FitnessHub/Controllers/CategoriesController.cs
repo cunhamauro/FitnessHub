b8f84533838ec3008112de4d07e36c3705fd835b
Diogo Sanlez <diogosdl25@gmail.com>
2024-09-26 16:52:45 +0100

    Crud Categories

---
+++ FitnessHub/FitnessHub/Controllers/CategoriesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FitnessHub.Data.Entities.GymMachines;
+using FitnessHub.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessHub.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        // GET: CategoriesController
+        public IActionResult Index()
+        {
+            var categories = _categoryRepository.GetAll().OrderBy(e => e.Name);
+            return View(categories);
+        }
+
+        // GET: CategoriesController/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _categoryRepository.GetByIdAsync(id.Value);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // GET: CategoriesController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryRepository.CreateAsync(category);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: CategoriesController/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _categoryRepository.GetByIdAsync(id.Value);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: CategoriesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryRepository.UpdateAsync(category);
+                }
+                catch (Exception)
+                {
+                    if (!await _categoryRepository.ExistsAsync(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _categoryRepository.GetByIdAsync(id.Value);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: CategoriesController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            await _categoryRepository.DeleteAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
